5046c9102e16046ddaef79bd120e1f0d2f80e4bb
Indiramohan Prakasam <pimohan.mca@gmail.com>
2024-12-07 11:59:32 +0530

    Routing added with default to landing page

--- src/index.js
+++ src/index.js
@@ -1,13 +1,37 @@
-import React from 'react';
+import React, { Suspense, lazy } from "react";
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
 
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { Spin } from "antd";
+import './index.css';
+
+const LandingPage = lazy(() => import("./pages/LandingPage"));
+
+const Loading = () => <Spin size="default" fullscreen />;
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <Suspense fallback={<Loading />}>
+        <LandingPage />  {/* SignIn Component : */}
+      </Suspense>
+    ),
+  },
+  {
+    path: "/login",
+    element: (
+      <Suspense fallback={<Loading />}>
+        <LandingPage /> {/* SignIn Component : */}
+      </Suspense>
+    ),
+  },
+]);
 
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={appRouter} />
   </React.StrictMode>
 );
 
