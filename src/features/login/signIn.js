7d6445b062e5258bf1e8b0ff953a81c489d6fe62
pallabidas22 <pallabi.22das@gmail.com>
2024-12-07 15:23:21 +0530

    Router change

--- src/features/login/signIn.js
+++ src/features/login/signIn.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, createContext,useContext } from 'react';
 import { useNavigate } from "react-router-dom";
 import { Paper, TextField, Button } from '@mui/material';
 import Grid from '@mui/material/Grid2';
@@ -38,27 +38,29 @@ const Login=()=>{
 
     const handleSubmit = async (event) => {
         event.preventDefault();
-        if(customerId !== "" && password !==""){
-            const response = authPost(customerId, password);
-            response.then((data)=>{
+        navigate("/accounts");
+        // if(customerId !== "" && password !==""){
+        //     const response = authPost(customerId, password);
+        //     response.then((data)=>{
                 
-                const customerIds = validCustomerId();
-                customerIds.then((res) =>{
-                    const validation = res.filter((results) => results.customerId === Number(data.customerId));
-                    if(validation.length > 1){
-                        navigate("/accounts", { state: { data: data, status: "200"} });
-                        setSeverity("success");
-                    }else{
-                        setOpen(true);
-                        setSeverity("error");
-                    }
-                })
-            })
-        }
-        else{
-            setOpen(true);
-            setSeverity("error");
-        }
+        //         const customerIds = validCustomerId();
+        //         customerIds.then((res) =>{
+        //             navigate("/accounts", { state: { data: data, status: "200"} });
+        //             const validation = res.filter((results) => results.customerId === Number(data.customerId));
+        //             if(validation.length > 1){
+        //                 navigate("/accounts", { state: { data: data, status: "200"} });
+        //                 setSeverity("success");
+        //             }else{
+        //                 setOpen(true);
+        //                 setSeverity("error");
+        //             }
+        //         })
+        //     })
+        // }
+        // else{
+        //     setOpen(true);
+        //     setSeverity("error");
+        // }
     };
 
     const handlePassword = (value) =>{
