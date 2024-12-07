16e6c4692049a34c1b27d5d8b7ebf356b16eec2a
Indiramohan Prakasam <pimohan.mca@gmail.com>
2024-12-07 13:23:21 +0530

    Layout added

--- src/index.js
+++ src/index.js
@@ -1,21 +1,21 @@
 import React, { Suspense, lazy } from "react";
-import ReactDOM from 'react-dom/client';
+import ReactDOM from "react-dom/client";
 
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import { Spin } from "antd";
-import './index.css';
+import "./index.css";
 
 const LandingPage = lazy(() => import("./pages/LandingPage"));
 
 const Loading = () => <Spin size="default" fullscreen />;
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
 const appRouter = createBrowserRouter([
   {
     path: "/",
     element: (
       <Suspense fallback={<Loading />}>
-        <LandingPage />  {/* SignIn Component : */}
+        <LandingPage /> {/* SignIn Component : */}
       </Suspense>
     ),
   },
@@ -34,4 +34,3 @@ root.render(
     <RouterProvider router={appRouter} />
   </React.StrictMode>
 );
-
