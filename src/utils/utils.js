aa42bf7aa3e36c1d2cd9c3c83ece2a575e5f9f60
Alex Pan Yong <alexpanyong@gmail.com>
2024-12-07 19:40:46 +0800

    Merge transfer

---
+++ src/utils/utils.js
@@ -0,0 +1,14 @@
+export const initialTransferForm = {
+  fromSavingsAccountNumber: "",
+  toMortgageAccountNumber: "",
+  mortgageAmount: "",
+  remarks: "",
+};
+
+export const LIMITS = {
+  REMARKS_LIMIT: 100,
+  REMARKS_MIN_LENGTH: 10,
+  REMARKS_MAX_LENGTH: 100,
+  SAVINGS_ACCOUNT_NUMBER_LENGTH: 10,
+  MORTAGE_ACCOUNT_NUMBER_LENGTH: 10,
+};
